8404c9c759e23d420084bc297b5ca8d1c425977f
a_scherbakov <a_scherbakov@ITS-n-scherbakov-a.itgroup.local>
2016-11-07 17:57:11 +0300

    Merge origin/master
    Conflicts:
    	src/main/java/th/network1/Network.java

--- PocerSimulyator/src/main/java/th/pocersimulyator/ConvertCard.java
+++ PocerSimulyator/src/main/java/th/pocersimulyator/ConvertCard.java
@@ -14,10 +14,10 @@ public class ConvertCard {
 
     private final String[] suit = {"Worms", "Diamonds", "Baptize", "Peaks"};
     private final String[] suitRus = {"Червы", "Бубны", "Крести", "Пики"};
-    private final String[] number = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "Q", "K","A"};
-    private final String[] numberRus = {"T", "2", "3", "4", "5", "6", "7", "8", "9", "10", "В", "Д", "К","Т"};
+    private final String[] number = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "Q", "K", "A"};
+    private final String[] numberRus = {"T", "2", "3", "4", "5", "6", "7", "8", "9", "10", "В", "Д", "К", "Т"};
     private final int intSuit[] = {0, 1, 2, 3};
-    private final String[] suitPic = {"♥","♦","♣","♠"};
+    private final String[] suitPic = {"♥", "♦", "♣", "♠"};
     private final int intNumber[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
     public String getSuit(int i) {
@@ -27,7 +27,8 @@ public class ConvertCard {
     public String getSuitRus(int i) {
         return suitRus[i];
     }
-        public String getSuitPic(int i) {
+
+    public String getSuitPic(int i) {
         return suitPic[i];
     }
 
@@ -40,9 +41,17 @@ public class ConvertCard {
     }
 
     public int getCode(int i1, int i2) {
-        return i1  + i2*13;
+        return i1 + i2 * 13;
     }
 
+    public String getCard(int code) {
+        String str = "";
+        int i1 = code % 13;
+        str = number[i1];
+        i1 = (code - i1) / 13;
+        str = str + suitRus[i1];
+        return str;
+    }
 
     /* public String suitToRus(String str1) {
         int i = 0;
